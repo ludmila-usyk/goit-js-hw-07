3ce5c47961624104c09d950fbb267872eb862ab2
Usyk <milausik@ukr.net>
2021-07-12 16:22:33 +0300

    task 5 fixed

--- js/repeta.js
+++ js/repeta.js
@@ -112,3 +112,28 @@
 // function onMouseMove(event) {
 //     console.log(event);
 // };
+
+
+
+
+
+// const refs = {
+//     input: document.getElementById('name-input'),
+//     span: document.getElementById('name-output'),
+// };
+
+// refs.input.addEventListener('input', onInputChange);
+
+// function onInputChange(event) {
+//     console.log(event.currentTarget.value);
+//     refs.span.textContent = event.currentTarget.value;
+// };
+
+
+
+
+
+
+
+
+
--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,15 @@
-const refs = {
-    input: document.getElementById('name-input'),
-    span: document.getElementById('name-output'),
-};
 
-refs.input.addEventListener('input', onInputChange);
+const inputEl = document.getElementById('name-input');
+const spanEl = document.getElementById('name-output');
+
+    inputEl.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    console.log(event.currentTarget.value);
-    refs.span.textContent = event.currentTarget.value;
+      if (inputEl.value === '') {
+        spanEl.innerHTML = 'незнакомец';
+  } else {
+    spanEl.innerHTML = inputEl.value;
+  }
 };
+console.log(spanEl);
+
